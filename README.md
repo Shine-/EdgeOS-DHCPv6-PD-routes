068206e2493c1d4ae8d89795ce4e01f63ddc0d63
Shine- <kjdshf@hjxgfds.zy>
2024-05-16 19:19:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# DHCPv6-PD Routes for Ubiquiti EdgeOS
+If you have ever tried to configure your Ubiquiti EdgeRouter as a DHCPv6-PD **server**, ie. assigning IPv6 prefixes to other, downlink routers, you may have noticed that while the PD assignment will work fine, clients connected to your downlink router will not have any IPv6 connectivity - despite getting assigned a valid IPv6 address by the downlink router.
+
+<details><summary>Example EdgeOS configuration snippet for DHCPv6-PD</summary>
+
+```
+interfaces {
+      ethernet eth0 {
+        address 2001:db8::1/112
+        description PD-Downlink
+        ipv6 {
+            dup-addr-detect-transmits 1
+            router-advert {
+                cur-hop-limit 64
+                link-mtu 0
+                managed-flag true
+                max-interval 600
+                other-config-flag true
+                prefix ::/64 {
+                    autonomous-flag false
+                    on-link-flag true
+                    valid-lifetime 2592000
+                }
+                reachable-time 0
+                retrans-timer 0
+                send-advert true
+            }
+        }
+        speed auto
+    }
+}
+
+service {
+    dhcpv6-server {
+        shared-network-name PD-Downlink {
+            name-server 2001:db8::1111
+            subnet 2001:db8::/64 {
+                address-range {
+                    start 2001:db8::2 {
+                        stop 2001:db8::fff
+                    }
+                }
+                prefix-delegation {
+                    start 2001:db8:0:100:: {
+                        stop 2001:db8:0:ff00:: {
+                            prefix-length 56
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+```
+</details>
+
+The reason for this is the DHCP server used by EdgeOS, ISC-DHCPd, which is simply incapable of modifying the routing table. Therefore, all routes need to be added or removed manually, which, in my opinion, completely defeats the dynamic approach called "DHCP"...
+
+[This](pdroutes.sh) script tries to automate the route addition for new leases and route deletion for expired leases.
+
+It comes with a few caveats, namely it can't use the preferred way of Link-Local routes (ISC-DHCPd doesn't log the Link-Local address of a DHCPv6 client), and it's impossible - at least on an EdgeRouter - to automatically invoke the script on DHCPv6 events like "lease" or "expire" (the particular ISC-DHCPd version used by EdgeOS doesn't support that yet for *DHCPv6*, only for DHCPv4). For more information, see the comments inside the script.
+
+# Prerequisites
+You will likely require a static or semi-static IPv6 uplink configuration, to build a static DHCPv6-PD downlink configuration like in the example above. At least I haven't succeeded in making EdgeOS use a dynamic (e.g. DHCPv6-PD) uplink configuration to build its downlink DHCPv6 configuration. Let me know if I'm mistaken...
+
+# Usage
+* Configure your Edgerouter as a DHCPv6-PD server on any downlink interface(s)
+* Connect downlink routers to your EdgeRouter that are configured for uplink DHCPv6-PD (e.g. any modern WiFi router)
+* Copy the script to your EdgeRouter and run it
+
+It will read the ISC-DHCPd leases file and add routes for all PDs that it can find a destination hop for. If you run the script again, it will also delete any obsolete routes to destinations whose lease(s) have expired.
+
+The script will print its progress and some debug information on screen, and log the same to `/tmp/delegated.log`. There are some additional commented-out debug messages that you may want to re-enable in case you run into problems.
+
+You may want to run the script automatically after the EdgeRouter reboots (after EdgeOS restores the DHCPv6 leases file from backup) by copying it to `/config/scripts/post-config.d`, and also add the script to the hourly cron-jobs in `/etc/cron.hourly`. Note that in this case, it may take **up to an hour** for the script to pick up new/expired DHCPv6 leases and add/remove the relevant routes. So you might even want to create an own cron-job that runs more often.
+
+If you find a way to make EdgeOS run the script trigger-based, e.g. on new or expiring DHCPv6 leases, let me know!
+
+It shouldn't be hard to adapt the script to other (non-EdgeOS) systems that are using ISC-DHCPd for DHCPv6, as these will most likely suffer from the same problem. The script should be able to run with little to no modification in `bash` and not too cut-down `ash` (BusyBox) variants.
+
+# License / Legal
+I'm releasing this script into the Public Domain. It's completely free as in "use, distribute and modify as much as you like, but don't blame me if it won't fit your needs".
+
+I assert that all code in this script is my own. Any code derived from other works that may have been used in earlier, unreleased versions of this script has been removed and/or replaced by my own code prior to the first public release of this script.
+
+# Bug reports and suggestions
+I've been using this script and its predecessors for many months now in my lab environment. The script has gone through several minor and major modifications and rewrites, some due to lack of knowledge about how ISC-DHCPd works, some due to unexpected behavior from certain DHCPv6 clients, and last but not least also due to stupidity on my side. By now, I consider the script "working rather well in my lab", but it may still show flaws once used in the wild...
+
+Feel free to contact me if you find any bugs or have fixes or additions to share. You may also simply fork this repository and proceed development with your own version of the script - whatever fits you best!
+
+I might stop using EdgeOS sooner or later, since it seems to have been abandoned by Ubiquiti anyway. In addition, ISC-DHCPd is EoL'd by now, so it will disappear from active use over time. But as long as I'm still using EdgeOS, I'm open for suggestions concerning this script!
---
+++ pdroutes.sh
@@ -0,0 +1,134 @@
+# Add downlink IPv6 routes for DHCPv6-PD leases on Ubiquiti EdgeOS (ISC-DHCPd)
+# Script version: 2024-05-15
+
+# Ubiquiti EdgeRouters with EdgeOS can be configured to delegate IPv6 prefixes
+# via DHCPv6 (IA-PD via DHCPv6-PD), in addition to assigning single IPv6
+# addresses (IA-NA) to DHCPv6 clients. However, when delegating prefixes,
+# EdgeOS won't add any routes towards the downlink routers that it delegated
+# prefixes to. This leaves the downlink IPv6 connectivity nonfunctional. These
+# downlink routes have to be maintained manually, which defeats the dynamic
+# approach of DHCPv6-PD.
+#
+# This script will try to automate maintenance of the required routes, by
+# reading the ISC-DHCPd leases file and trying to add the required downlink
+# routes, resp. deleting previous routes for PD leases that have expired.
+#
+# Known quirks:
+# - Link-local routing is not supported, because ISC-DHCPd doesn't log the
+#   link-local address of a client that requested a DHCPv6 lease.
+# - Clients that only request a PD lease and no NA lease aren't supported.
+# - Clients that use a different DUID to request their NA vs. their PD lease
+#   aren't supported, but may be fuzzy-matched if their behavior is known.
+#   Currently implemented for recent Linux- and VxWorks-based TP-Link WiFi
+#   routers.
+# - The script needs to be run manually or on a schedule, because the
+#   particular ISC-DHCPd version used in EdgeOS doesn't support invoking
+#   external programs on DHCPv6 events.
+# - Neither current or old routes, nor the logs of this script are stored
+#   persistently. This is on purpose. The script is supposed to be re-run after
+#   each EdgeRouter reboot in order to recover downlink IPv6 connectivity.
+#
+# This script is tested on EdgeOS 1.10.11 and 2.0.9. It may or may not be
+# usable with other EdgeOS versions or other ISC-DHCPd equipped systems.
+
+# This script is Public Domain. You may freely use, modify and distribute it
+# in whole or in part, for any purpose, with or without crediting me.
+
+# No expressed or implied warranties of any kind. This script may or may not
+# serve any purpose. It's provided solely for educational and experimental use.
+
+# Please find the latest version of this script at:
+# https://github.com/Shine-/EdgeOS-DHCPv6-PD-routes
+
+
+# Set up logging and log rotation
+exec 3>&1 1> >(tee -a /tmp/delegated.log) 2>&1
+[ -f /tmp/delegated.log.bak ] || touch /tmp/delegated.log.bak
+rotate=$(($(date +%s) - $(date -r /tmp/delegated.log.bak +%s)))
+echo "---" `date +%Y-%m-%d' '%H:%M:%S` "--- Log file backup is $rotate seconds old."
+[ "$rotate" -gt 86400 -a -f /tmp/delegated.log ] && {
+	echo - Rotating logs and restarting.
+	mv /tmp/delegated.log /tmp/delegated.log.bak
+	# Stop logging to old file and restart script
+	exec $0 "$@" 1>&3 3>&-
+}
+
+echo "---" `date +%Y-%m-%d' '%H:%M:%S` "--- Starting setup/cleanup of routes for delegated prefixes"
+# Set up the arrays and hash lists we need
+declare -A NEXTHOP; declare -A ADDRESS
+declare -a LEASES; declare -a ROUTES; declare -a DELEGATED
+# Pull all active leases from the ISC-DHCPd leases file using AWK. ISC-DHCPd stores the DHCPv6 DUID as an escaped string,
+# which we need to fix up a bit, in order to prevent malfunction of this script, and for proper unescaping later.
+readarray -t LEASES < <(
+awk 'BEGIN { RS="\nia-"; FS=";\n"; }
+/^na|^pd/ {
+	for(i=1;i<=NF;i++) {
+		# since we are splitting at quotes, replace any actual escaped quote with its octal equivalent
+		if ($i ~ /na|pd/) { gsub(/\\\"/,"\\042",$i); split($i,DUID,"\"") }
+		if ($i ~ /iaprefix/) { split($i,ADDR," ") }
+		if ($i ~ /iaaddr/) { split($i,ADDR," ") }
+	}
+	if (ADDR[6] == "active") {
+		# a space character in DUID hurts us, use its octal equivalent instead
+		gsub(" ","\\040",DUID[2])
+		# octal character codes are 3-digits, any 4th digit is an actual number - escape it to its ASCII code
+		DUID[2] = gensub(/(\\[0-9]{3})([0-9])/,"\\1\\\\x3\\2","g",DUID[2])
+		printf "%s %s %s\n",DUID[1],ADDR[2],DUID[2]
+	}
+	delete DUID; delete ADDR
+} ' "/var/run/dhcpdv6.leases"
+)
+#declare -p LEASES
+[ ${#LEASES[@]} -ne 0 ] && {
+	# Now let's build a list of routes from the leases we parsed
+	echo "- We have the following active leases:"
+	for item in "${LEASES[@]}"; do 
+		set -- $item
+#		echo "Item: $item"
+#		echo "IA-${1^^} $2 delegated to $3"
+		# Convert ISC-DHCPd DUID format (escaped string) to hex, skipping the first byte due to TP-Link brokenness.
+		# Explanation: on TP-Link devices, the DUID used for requesting PD resp. NA may differ in the 1st byte (either x+1 or x-1)
+		# As the first byte designates the DUID type only (e.g. DUID-LLT or DUID-EN), skipping it shouldn't hurt other clients.
+		#D=$(printf '%b' "$3" | hexdump -s 1 -ve '1/1 "%02x"') # Attn: this is causing invalid seek operation errors in hexdump
+		D=$(printf '%b' "$3" | hexdump -ve '1/1 "%02x"'); D=${D:2}
+#		echo "DUID $3 == $D"
+		[ "$1" = "pd" ] && { echo "Prefix $2 delegated to DUID $D"; }
+		[ "$1" = "na" ] && { echo "Address $2 leased to DUID $D"; }
+		[ "$1" = "na" ] && { ADDRESS["$D"]="$2"; }
+		[ "$1" = "pd" ] && { NEXTHOP["$2"]="$D"; }
+	done
+#	echo "- We have active prefix delegations to the following devices:"
+#	printf '%s\n' "${NEXTHOP[@]}"
+#	echo "- To which we're delegating the following prefixes:"
+#	printf '%s\n' "${!NEXTHOP[@]}"
+	echo "- We currently need the following routes:"
+	for prefix in ${!NEXTHOP[@]}; do
+		VIA="${ADDRESS["${NEXTHOP["$prefix"]}"]}"
+		[ -n "$VIA" ] && {
+			ROUTE="$prefix via $VIA"; ROUTES+=("$ROUTE")
+			echo "$ROUTE"
+		} || echo "# $prefix (ignoring, no DHCPv6 lease found for DUID ${NEXTHOP["$prefix"]})"
+	done
+	echo "- Database of routes for prefixes we've delegated before:"
+	[ -f "/tmp/delegated.db" ] && { readarray -t DELEGATED < /tmp/delegated.db; } || echo "<< empty >>"
+	for item in "${DELEGATED[@]}"; do echo $item; done
+	echo "- Checking for routes we don't need anymore (deleting as necessary):"
+	for item in "${DELEGATED[@]}"; do
+		itemx="\<${item}\>"
+		[[ ${ROUTES[@]} =~ $itemx ]] && { echo "# $item (still delegating)"; } || {
+			echo "Deleting route: $item"
+			ip -6 r del $item
+		}
+	done
+	echo "- Checking for missing routes (adding as necessary):"
+	for ROUTE in "${ROUTES[@]}"; do
+		[ -n "$(ip -6 r | grep "$ROUTE")" ] && { echo "# $ROUTE (already present)"; } || {
+			echo "Adding route: $ROUTE"
+			ip -6 r add $ROUTE
+		}
+	done
+	echo "- Writing current routes for delegated prefixes to database:"
+	{ for item in "${ROUTES[@]}"; do echo $item; done; } > /tmp/delegated.db
+	for item in "${ROUTES[@]}"; do echo $item; done
+}
+echo "---" `date +%Y-%m-%d' '%H:%M:%S` "--- Finished setup/cleanup of routes for delegated prefixes"
